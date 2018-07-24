154964b7f1436d4702390c636f66f1630fc07a7b
Hersenberg <1415637345@qq.com>
2018-07-24 20:10:42 +0800

    sc

--- explorer/explorer_joy_control/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "explorer_joy_control.h"
-
-int main(int argc , char **argv) {
-    ros::init(argc , argv, "joy_control") ;
-    ros::NodeHandle node;
-    ExplorerTeleop explorer_teleop;
-    ros::spin();
-}
-
